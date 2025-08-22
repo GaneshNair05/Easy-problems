20df817771bd856134481a5f4b0e2c4fcc4959fd
GaneshNair05 <sman4519@gmail.com>
2025-08-22 16:25:32 +0530

    My 21st problem

---
+++ 021_print_natural_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+
+int main() {
+  
+    cout << "The first ten natural numbers are:" ;
+
+    for (int i = 1; i <= 10; ++i) {
+        cout <<endl<< i << endl;                          //Print natural numbers
+    }
+
+    return 0; 
+}
